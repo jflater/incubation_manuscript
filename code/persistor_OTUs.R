671e69d887fe0165ce7b78c4f044485c28f660ef
Jared Flater <jflater@gmail.com>
2018-12-19 10:44:30 -0600

    update

--- code/persistor_OTUs.R
+++ code/persistor_OTUs.R
@@ -1,51 +1,54 @@
 library(tidyverse)
 library(gplots)
+
 # Read in whole phyloseq object
 incubation.physeq <- readRDS("data/RDS/incubation_physeq_Aug18.RDS")
 
-GetOTUs <- function(physeq, samplestogetotusfrom) {
-  subset_samples(physeq, treatment == "samplestogetotusfrom") %>%
+######
+# Working on function to get a list of OTUs from a sample type
+GetOTUs <- function(z, y) {
+    subset_samples(z, treatment == as.character(y)) %>%
     filter_taxa(function(x) sum(x) > 0, T) %>%
     tax_table() %>%
     row.names()
 }
 
+# Testing the function
 alfalfa.amendment.otus <- GetOTUs(incubation.physeq, "Alfalfa")
+#####
 
 # Make new object with only samples from the inputs
 inputs <- subset_samples(incubation.physeq, treatment %in% c("AlfalfaAmend", "AlfalfaSoil", "CompostAmend"))
 
 # Get incubated alfalfa samples
-alfala.physeq <- subset_samples(incubation.physeq, treatment == c("Alfalfa")) %>%
-  filter_taxa(function(x) sum(x) > 0, T) 
-
-# get OTUs from alfalfa samples
-alfala.incubated.otus <- alfala.physeq %>% tax_table() %>%
+alfala.incubated.otus <- subset_samples(incubation.physeq, treatment == "Alfalfa") %>%
+  filter_taxa(function(x) sum(x) > 0, T) %>%
+  tax_table() %>%
   row.names()
 
+# Get OTUs from alfalfa amendment
 alfalfa.amendment.otus <- subset_samples(inputs, treatment == "AlfalfaAmend") %>%
   filter_taxa(function(x) sum(x) > 0, T) %>%
   tax_table() %>%
   row.names()
 
+# Get OTUs from compost amendment
 compost.amendment.otus <- subset_samples(inputs, treatment == "CompostAmend") %>%
   filter_taxa(function(x) sum(x) > 0, T) %>%
   tax_table() %>%
   row.names()
 
+# Get OTUs from the starting soil
 soil.amendment.otus <- subset_samples(inputs, treatment == "AlfalfaSoil") %>%
   filter_taxa(function(x) sum(x) > 0, T) %>%
   tax_table() %>%
   row.names()
 
+# Make list of OTUs to use in venn()
 otus <- list(alfalfa.amendment.otus, compost.amendment.otus, soil.amendment.otus)
 venn <- venn(otus)
 
-common.otus <- attr(venn,"intersections")$`A:B:C`
 only.alfa.input <- attr(venn, "intersections")$`A`
-ex1 = prune_taxa(common.otus, inputs)
-tax_table(ex1)
-plot_heatmap(ex1)
 
 # Alfalfa treated soil and alfalfa amendment
 alfa.otus <- list(only.alfa.input, alfala.physeq.input)
