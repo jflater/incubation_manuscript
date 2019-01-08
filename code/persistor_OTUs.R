a5f0d9f43c2928221fc9d5de71e56d8a4b500f83
Jared Flater <jflater@gmail.com>
2019-01-08 13:44:02 -0600

    update

--- code/persistor_OTUs.R
+++ code/persistor_OTUs.R
@@ -3,61 +3,35 @@ library(gplots)
 
 # Read in whole phyloseq object
 incubation.physeq <- readRDS("data/RDS/incubation_physeq_Aug18.RDS")
-
+levels(sample_data(incubation.physeq)$treatment)
 ######
 # Working on function to get a list of OTUs from a sample type
-GetOTUs <- function(z, y) {
-    prune_samples(sample_data(z)$treatment %in% c(y), z) %>%
+GetOTUs <- function(physeq, samples) {
+    prune_samples(sample_data(physeq)$treatment %in% c(samples), physeq) %>%
     filter_taxa(function(x) sum(x) > 0, T) %>%
     tax_table() %>%
     row.names()
 }
 
-test <- GetOTUs(incubation.physeq, "Alfalfa")
+incubated.Alfalfa.otus <- GetOTUs(incubation.physeq, c("Alfalfa"))
+incubated.Compost.otus <- GetOTUs(incubation.physeq, c("Compost"))
+incubated.CompAlfa.otus <- GetOTUs(incubation.physeq, c("CompAlfa"))
+incubated.Control.otus <- GetOTUs(incubation.physeq, c("Control"))
+incubated.AlfalfaAmend.otus <- GetOTUs(incubation.physeq, c("AlfalfaAmend"))
+incubated.CompostAmend.otus <- GetOTUs(incubation.physeq, c("CompostAmend"))
+incubated.AlfalfaSoil.otus <- GetOTUs(incubation.physeq, c("AlfalfaSoil"))
 
-# Testing the function
-alfalfa.amendment.otus <- GetOTUs(incubation.physeq, "Alfalfa")
-z = incubation.physeq
-y = "Alfalfa"
-subset_samples(z, treatment == y) %>%
-  filter_taxa(function(x) sum(x) > 0, T) %>%
-  tax_table() %>%
-  row.names()
-
-
+otus <- list(incubated.AlfalfaAmend.otus, incubated.CompostAmend.otus, incubated.AlfalfaSoil.otus)
+venn <- venn(otus)
 #####
 
-# Make new object with only samples from the inputs
-inputs <- prune_samples(sample_data(incubation.physeq)$treatment %in% c("AlfalfaAmend", "AlfalfaSoil", "CompostAmend"), incubation.physeq)
+# OTUs from the alfalfa amendment, not in soil
+a <- list(incubated.Control.otus)
+b <- list(incubated.AlfalfaSoil.otus)
+c <- mapply(c, a, b, SIMPLIFY = F)
+soilplusrefotus <- list(incubated.Control.otus, incubated.AlfalfaSoil.otus)
 
-# Get incubated alfalfa samples
-alfala.incubated.otus <- subset_samples(incubation.physeq, treatment == "Alfalfa") %>%
-  filter_taxa(function(x) sum(x) > 0, T) %>%
-  tax_table() %>%
-  row.names()
-
-# Get OTUs from alfalfa amendment
-alfalfa.amendment.otus <- subset_samples(inputs, treatment == "AlfalfaAmend") %>%
-  filter_taxa(function(x) sum(x) > 0, T) %>%
-  tax_table() %>%
-  row.names()
-
-# Get OTUs from compost amendment
-compost.amendment.otus <- subset_samples(inputs, treatment == "CompostAmend") %>%
-  filter_taxa(function(x) sum(x) > 0, T) %>%
-  tax_table() %>%
-  row.names()
-
-# Get OTUs from the starting soil
-soil.amendment.otus <- subset_samples(inputs, treatment == "AlfalfaSoil") %>%
-  filter_taxa(function(x) sum(x) > 0, T) %>%
-  tax_table() %>%
-  row.names()
-
-# Make list of OTUs to use in venn()
-otus <- list(alfalfa.amendment.otus, compost.amendment.otus, soil.amendment.otus)
-venn <- venn(otus)
-
+alfminussoil <- list()
 only.alfa.input <- attr(venn, "intersections")$`A`
 
 # Alfalfa treated soil and alfalfa amendment
