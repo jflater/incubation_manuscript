c577157b1a2eac7ee12e3e81746cc09447e0af69
Jared Flater <jflater@gmail.com>
2019-01-23 13:48:08 -0600

    alien work

--- code/persistor_OTUs.R
+++ code/persistor_OTUs.R
@@ -1,45 +1,48 @@
+library(phyloseq)
 library(tidyverse)
 library(gplots)
 
 # Read in whole phyloseq object
 incubation.physeq <- readRDS("data/RDS/incubation_physeq_Aug18.RDS")
+
+# See what catgegories describe our treatments:
 levels(sample_data(incubation.physeq)$treatment)
-######
-# Working on function to get a list of OTUs from a sample type
+
+# Function to get a list of OTUs from a sample type
 GetOTUs <- function(physeq, samples) {
     prune_samples(sample_data(physeq)$treatment %in% c(samples), physeq) %>%
-    filter_taxa(function(x) sum(x) > 0, T) %>%
+    filter_taxa(function(x) sum(x) > 1, T) %>%
     tax_table() %>%
     row.names()
 }
 
-incubated.Alfalfa.otus <- GetOTUs(incubation.physeq, c("Alfalfa"))
-incubated.Compost.otus <- GetOTUs(incubation.physeq, c("Compost"))
-incubated.CompAlfa.otus <- GetOTUs(incubation.physeq, c("CompAlfa"))
-incubated.Control.otus <- GetOTUs(incubation.physeq, c("Control"))
-incubated.AlfalfaAmend.otus <- GetOTUs(incubation.physeq, c("AlfalfaAmend"))
-incubated.CompostAmend.otus <- GetOTUs(incubation.physeq, c("CompostAmend"))
-incubated.AlfalfaSoil.otus <- GetOTUs(incubation.physeq, c("AlfalfaSoil"))
+# Using function to generate list of OTUs from all starting soil, incubated soils and amendments
+# Incubated samples:
+Alfalfa.otus <- GetOTUs(incubation.physeq, c("Alfalfa"))
+Compost.otus <- GetOTUs(incubation.physeq, c("Compost"))
+CompAlfa.otus <- GetOTUs(incubation.physeq, c("CompAlfa"))
+Control.otus <- GetOTUs(incubation.physeq, c("Control"))
+# Amendment samples:
+AlfalfaAmend.otus <- GetOTUs(incubation.physeq, c("AlfalfaAmend"))
+CompostAmend.otus <- GetOTUs(incubation.physeq, c("CompostAmend"))
+# Soil sample:
+AlfalfaSoil.otus <- GetOTUs(incubation.physeq, c("AlfalfaSoil"))
 
-otus <- list(incubated.AlfalfaAmend.otus, incubated.CompostAmend.otus, incubated.AlfalfaSoil.otus)
+
+# Venn diagram for identifying OTUs unique to alfalfa amendment that were not detected in the incubated control soils
+otus <- list(AlfalfaAmend.otus, Control.otus)
 venn <- venn(otus)
-#####
 
-# OTUs from the alfalfa amendment, not in soil
-a <- list(incubated.Control.otus)
-b <- list(incubated.AlfalfaSoil.otus)
-c <- mapply(c, a, b, SIMPLIFY = F)
-soilplusrefotus <- list(incubated.Control.otus, incubated.AlfalfaSoil.otus)
+# OTUs in alfalfa amendment but not detected in incubated control samples are candidates for "Alien" status, or thos otus
+# that transfer from the amendment to the soil and were not present prior amendment of soil
+alf.aliens <- attr(venn, "intersections")$A
 
-alfminussoil <- list()
-only.alfa.input <- attr(venn, "intersections")$`A`
+# compare aliens to incubated alfalfa samples
+aliens <- list(alf.aliens, Alfalfa.otus)
+aliens.venn <- venn(aliens)
 
-# Alfalfa treated soil and alfalfa amendment
-alfa.otus <- list(only.alfa.input, alfala.physeq.input)
-alf.venn <- venn(alfa.otus)
-
-ex2 = prune_taxa(attr(alf.venn, "intersections")$`A`, alfala.physeq) %>%
-  filter_taxa(function(x) sum(x) > 0, T)
+# aliens detected!
+alf.aliens.detected <- attr(aliens.venn,"intersections")$`A:B`
 
 
   
